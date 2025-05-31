3cb676ff7b9d54dd3b661656a5e6ba0e69e6f6ac
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-05-31 00:23:21 -0700

    push

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
    <div className="flex justify-center items-center flex-col gap-4 text-white h-[44vh]">
-    <div className="font-bold text-5xl">Buy Me a Biryani</div>
+    <div className="font-bold flex items-center gap-2 text-5xl">Buy Me a Biryani <span><img src="/biryani.png" width={80} alt="" /></span></div>
     <p>It’s an easy way to raise some funds and share a little love just like how biryani brings people together, we bring support to your passion.</p>
     <div className="">
       <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
