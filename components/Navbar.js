2daacfd1d92309995e1ece2adc91c9b899ce2aa0
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-06-03 21:54:15 -0700

    push

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,24 +6,12 @@ import Link from 'next/link'
 const Navbar = () => {
   const { data: session } = useSession()
   const [showdropdown, setShowdropdown] = useState(false)
-  if (session) {
-    // return <>
-    //   Signed in as {session.user.email} <br/>
-    //   <button onClick={() => signOut()}>Sign out</button>
-    // </>
-  }
+
   return (<nav className='bg-[#020D25] text-white flex justify-between px-4 items-center h-16'>
     <div className="logo font-bold text-lg flex items-center justify-center">
       <img src="/biryani.png" width={44} alt="" />
       <span className='bg-gradient-to-r from-sky-200 via-blue-100 to-slate-300 text-transparent bg-clip-text'>GetMeABiryani</span>
     </div>
-    {/* <ul className="flex justify-between gap-4">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul> */}
 
     <div className="relative">
       {session && <>
@@ -34,22 +22,21 @@ const Navbar = () => {
         <div id="dropdown" className={`z-10 ${showdropdown?"":"hidden"} absolute left-[125px] bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700`}>
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+              <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
             </li>
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+              <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
             </li>
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+              <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</Link>
             </li>
             <li>
-              <a onClick={()=> signOut()} href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+              <Link onClick={()=> signOut()} href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
             </li>
           </ul>
         </div>
       </>}
-      {session && <Link href={"/dashboard"}><button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Dashboard</button></Link>}
-      {session && <button onClick={() => { signOut() }} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>}
+     
       {!session && <Link href={"/login"}>
         <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button> </Link>}
     </div>
