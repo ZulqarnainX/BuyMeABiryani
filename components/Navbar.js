d5f4a4d461931d1cd4c4d08aafe328c40a18f2d5
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-06-03 21:31:53 -0700

    push

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,10 +6,10 @@ import Link from 'next/link'
 const Navbar = () => {
   const { data: session } = useSession()
   if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
+    // return <>
+    //   Signed in as {session.user.email} <br/>
+    //   <button onClick={() => signOut()}>Sign out</button>
+    // </>
   }
   return ( <nav className='bg-[#020D25] text-white flex justify-between px-4 items-center h-16'>
       <div className="logo font-bold text-lg flex items-center justify-center">
@@ -25,8 +25,11 @@ const Navbar = () => {
       </ul> */}
 
       <div>
-        <Link href={"/login"}>
-        <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button> </Link>
+        {session && <Link href={"/dashboard"}><button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Dashboard</button></Link>}
+        {session &&<button onClick={()=>{signOut()}} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>}
+
+        {!session && <Link href={"/login"}>
+        <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button> </Link>}
       </div>
       </nav>
   )
